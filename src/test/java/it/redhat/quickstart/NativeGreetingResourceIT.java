6e2a7cbf5464b954e028eab14af55864dcca25db
Giuseppe Bonocore <gbonocor@redhat.com>
2019-05-19 12:28:51 +0200

    native and uberjar tests

--- src/test/java/it/redhat/quickstart/NativeGreetingResourceIT.java
+++
@@ -1,9 +0,0 @@
-package it.redhat.quickstart;
-
-import io.quarkus.test.junit.SubstrateTest;
-
-@SubstrateTest
-public class NativeGreetingResourceIT extends GreetingResourceTest {
-
-    // Execute the same tests but in native mode.
-}
